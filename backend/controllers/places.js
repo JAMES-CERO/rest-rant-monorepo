d6ba69c001140d188dc2919ca2327dd6f8963304
James Aules <james_aules@icloud.com>
2023-05-01 20:16:49 -0400

    attach logged-in user as comment author

--- backend/controllers/places.js
+++ backend/controllers/places.js
@@ -94,16 +94,26 @@ router.post('/:placeId/comments', async (req, res) => {
         res.status(404).json({ message: `Could not find place with id "${placeId}"` })
     }
 
-    const author = await User.findOne({
-        where: { userId: req.body.authorId }
-    })
+    let currentUser;
+    try {
+        let currentUser = await User.findOne({
+            where: {
+                userId: req.session.userId
+            } 
+        })
+    } catch {
+        currentUser(null)
+    }
 
-    if (!author) {
-        res.status(404).json({ message: `Could not find author with id "${req.body.authorId}"` })
+    if(!currentUser) {
+        return res.status(404).json({
+            message: 'You must be logged in to do this action'
+        })
     }
 
     const comment = await Comment.create({
         ...req.body,
+        authorId: currentUser.userId ,
         placeId: placeId
     })
 
