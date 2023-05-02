c5df7fdaa057e8ec5fdec36da37f726d7438fdd4
James Aules <james_aules@icloud.com>
2023-05-01 23:49:43 -0400

    delete buttom conditional

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,7 @@ const defineCurrentUser = require('./middleware/defineCurrentUser')
 app.use(cookieSession({
     name: 'session',
     keys: [process.env.SESSION_SECRET ],
+    sameSite: 'stric',
     maxAge: 24 * 60 * 60 * 1000 // 24 h
 }))
 app.use(cors({
